f546e3dcbaf2d7c860b02573953b4a94c5ab0c7c
Viettranni <127795338+Viettranni@users.noreply.github.com>
2023-10-31 04:38:59 +0200

    Update

---
+++ Moduuli1/tehtävä7/tehtava7.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let amountRolls = prompt('Enter the amount of the Rolls!')
+amountRolls = parseInt(amountRolls)
+
+// Let's say that the roll has 6 faces
+function rollDie() {
+  return Math.floor(Math.random()*6)+1;
+}
+
+// Throw the requested amount of rolls and add the rolls into sum
+let sum = 0;
+for (let i = 0; i < amountRolls; i++) {
+  const rollResult = rollDie();
+  sum +=rollResult
+}
+
+let finalAnswer = ` Rolls counted: ${sum} `
+
+document.querySelector('#target').innerHTML = finalAnswer;
