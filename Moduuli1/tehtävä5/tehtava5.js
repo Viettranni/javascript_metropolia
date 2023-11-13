a2c395dcfac2f907529ea581d4aa2f9ade284b49
Viettranni <127795338+Viettranni@users.noreply.github.com>
2023-11-13 11:40:41 +0200

    Update

--- Moduuli1/tehtävä5/tehtava5.js
+++ Moduuli1/tehtävä5/tehtava5.js
@@ -4,7 +4,7 @@ let enterYear = parseInt(prompt('Enter a Year!'));
 
 let finalAnswer;
 
-if ((enterYear % 100 === 0 && enterYear % 400 === 0) || (enterYear % 4 === 0 && enterYear % 100 !== 0)) {
+if ((enterYear % 4 === 0 && enterYear % 100 !== 0) || enterYear % 400 === 0) {
   finalAnswer = `The ${enterYear} Year is a leap`;
 } else {
   finalAnswer = `The ${enterYear} Year is a non-leap`;
